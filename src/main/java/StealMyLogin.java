21f7e5a6633501a56420e8ef995d648fae054ebd
BlagojeJanjic <blagojejanjic96@gmail.com>
2022-05-28 21:40:52 +0200

    Provera novog url-a

--- src/main/java/StealMyLogin.java
+++ src/main/java/StealMyLogin.java
@@ -5,34 +5,40 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StealMyLogin {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        try {
+            System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
 
-        driver.manage().window().maximize();
+            driver.manage().window().maximize();
 
-        driver.navigate().to("https://www.stealmylogin.com/demo.html");
+            driver.navigate().to("https://www.stealmylogin.com/demo.html");
 
-        WebElement loginField = driver.findElement(By.xpath("/html/body/form/input[1]"));
+            WebElement loginField = driver.findElement(By.xpath("/html/body/form/input[1]"));
 
-        loginField.sendKeys("ajhsdjh");
+            loginField.sendKeys("ajhsdjh");
 
-        WebElement passwordField = driver.findElement(By.xpath("/html/body/form/input[2]"));
+            WebElement passwordField = driver.findElement(By.xpath("/html/body/form/input[2]"));
 
-        passwordField.sendKeys("123asdf");
+            passwordField.sendKeys("123asdf");
 
-        WebElement loginButton = driver.findElement(By.xpath("/html/body/form/input[3]"));
+            WebElement loginButton = driver.findElement(By.xpath("/html/body/form/input[3]"));
 
-        loginButton.click();
+            loginButton.click();
 
-        String newUrl = driver.getCurrentUrl();
+            String newUrl = driver.getCurrentUrl();
 
-        if(newUrl.equals("https://example.com/")){
-            System.out.println("Nice");
-        } else {
-            System.out.println("Not nice");
+            if (newUrl.equals("https://example.com/")) {
+                System.out.println("Nice");
+            } else {
+                System.out.println("Not nice");
+            }
+
+            Thread.sleep(5000);
+
+            driver.close();
+
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
-
-        driver.close();
-
     }
 }
